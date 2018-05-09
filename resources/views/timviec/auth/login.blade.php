c7933d0764ffebb2ac27dffe159fb0788c787d90
Lê Xuân Trường <xuantruong.220494@gmail.com>
2018-05-09 17:23:04 +0700

    edit text

--- resources/views/timviec/auth/login.blade.php
+++ resources/views/timviec/auth/login.blade.php
@@ -68,7 +68,7 @@
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">Login</button>
                                 <a class="btn btn-link" href="{{ route('timviec.password.request') }}">
-                                    Forgot Your Password? quên mật khẩu
+                                    Forgot Your Password?
                                 </a>
                             </div>
                         </div>
